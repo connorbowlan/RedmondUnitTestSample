1ba63ad6cc5a14609e238a99609b446472e683e9
Connor Bowlan <cbowlan@tbb.bank>
2022-08-10 13:27:19 -0500

    Added new functionality to Calculator and initilized a SUT in singleton pattern.

--- RedmondUnitTestSample/UnitTest1.cs
+++ RedmondUnitTestSample/UnitTest1.cs
@@ -5,6 +5,17 @@ namespace RedmondUnitTestSample
     [TestClass]
     public class CalculatorFixture
     {
+        [TestInitialize]
+        public void OnTestInitialize()
+        {
+            _calculator = null;
+        }
+
+        private Calculator? _calculator;
+
+        // This is a singleton pattern class.
+        public Calculator SystemUnderTest => _calculator ??= new Calculator();
+
         [TestMethod]
         public void Add()
         {
@@ -15,7 +26,7 @@ namespace RedmondUnitTestSample
             var sut = new Calculator();
 
             // The expected returned value.
-            var expected = 4;
+            var expected = 5;
 
             // Act
             // The actual returned value.
@@ -29,19 +40,64 @@ namespace RedmondUnitTestSample
         [TestMethod]
         public void Subtract()
         {
-            Assert.Inconclusive();
+            // Arrange
+            var value1 = 2;
+            var value2 = 2;
+
+            var sut = new Calculator();
+
+            // The expected returned value.
+            var expected = 0;
+
+            // Act
+            // The actual returned value.
+            var actual = sut.Subtract(value1, value2);
+
+            // Assert
+            // An assertion that the values are equal.
+            Assert.AreEqual<int>(expected, actual);
         }
 
         [TestMethod]
         public void Multiply()
         {
-            Assert.Inconclusive();
+            // Arrange
+            var value1 = 6;
+            var value2 = 3;
+
+            var sut = new Calculator();
+
+            // The expected returned value.
+            var expected = 18;
+
+            // Act
+            // The actual returned value.
+            var actual = sut.Multiply(value1, value2);
+
+            // Assert
+            // An assertion that the values are equal.
+            Assert.AreEqual<int>(expected, actual);
         }
 
         [TestMethod]
         public void Divide()
         {
-            Assert.Inconclusive();
+            // Arrange
+            var value1 = 10;
+            var value2 = 5;
+
+            var sut = new Calculator();
+
+            // The expected returned value.
+            var expected = 2;
+
+            // Act
+            // The actual returned value.
+            var actual = sut.Divide(value1, value2);
+
+            // Assert
+            // An assertion that the values are equal.
+            Assert.AreEqual<int>(expected, actual);
         }
     }
 }
--- VsLiveRedmond.Api/Calculator.cs
+++ VsLiveRedmond.Api/Calculator.cs
@@ -6,5 +6,20 @@
         {
             return value1 + value2;
         }
+
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        public int Divide(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
     }
 }
